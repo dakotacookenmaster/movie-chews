bd34a6748cc00f2b245759982ac0b13110686c33
Dakota Cookenmaster <dakotacookenmaster@southern.edu>
2022-09-10 12:31:43 -0400

    Removed superfluous console.log

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.MC_NESTJS_API_PORT)
   await app.listen(process.env.MC_NESTJS_API_PORT || 3000, '0.0.0.0');
 }
 bootstrap();
